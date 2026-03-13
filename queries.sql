307cc04b1baf4c05aeb435ef5e0146efa81271ba
The-Chiru <rahulchiru76@gmail.com>
2026-03-13 09:39:54 +0530

    queries.sql

--- queries.sql
+++ queries.sql
@@ -1,38 +1,34 @@
--- 1 Highest average rating movie
-
-SELECT m.title, AVG(r.rating) AS avg_rating
+-- Question 1: Highest average rating movie
+SELECT m.titel, AVG(r.rating) AS avg_rating
 FROM movies m
-JOIN ratings r ON m.movie_id = r.movie_id
-GROUP BY m.title
+JOIN ratings r
+ON m.movie_id = r.movieID
+GROUP BY m.titel
 ORDER BY avg_rating DESC
 LIMIT 1;
 
 
--- 2 Top 5 genres by average rating
-
-SELECT g.name, AVG(r.rating) AS avg_rating
-FROM genres g
-JOIN movie_genres mg ON g.genre_id = mg.genre_id
-JOIN ratings r ON mg.movie_id = r.movie_id
-GROUP BY g.name
+-- Question 2: Top 5 genres
+SELECT m.geners, AVG(r.rating) AS avg_rating
+FROM movies m
+JOIN ratings r
+ON m.movie_id = r.movieID
+GROUP BY m.geners
 ORDER BY avg_rating DESC
 LIMIT 5;
 
 
--- 3 Director with most movies
-
-SELECT director, COUNT(*) AS movie_count
+--Director with most movies
+SELECT Director, COUNT(*) AS movie_count
 FROM movies
-WHERE director IS NOT NULL
 GROUP BY director
 ORDER BY movie_count DESC
 LIMIT 1;
 
-
--- 4 Average rating per year
-
-SELECT m.release_year, AVG(r.rating) AS avg_rating
+--Average rating per year
+SELECT YEAR ,AVG(r.rating)
 FROM movies m
-JOIN ratings r ON m.movie_id = r.movie_id
-GROUP BY m.release_year
-ORDER BY m.release_year;
+JOIN ratings r
+ON m.movie_id = r.movieID
+GROUP BY YEAR
+ORDER BY YEAR;
