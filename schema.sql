8610a8383e1136d081999a90008b688da9275ca3
The-Chiru <rahulchiru76@gmail.com>
2026-03-13 09:39:03 +0530

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,33 +1,15 @@
-TRUNCATE TABLE movie_genres, ratings, genres, movies RESTART IDENTITY CASCADE;
-
-CREATE TABLE movies (
-    movie_id INT PRIMARY KEY,
-    title TEXT,
-    release_year INT,
+CREATE TABLE IF NOT EXISTS movies(
+    movie_id INTEGER PRIMARY KEY,
+    titel TEXT,
+    year INTEGER,
+    geners TEXT,
     director TEXT,
-    plot TEXT,
-    imdb_id TEXT,
+    plot text,
     box_office TEXT
 );
-
-CREATE TABLE genres (
-    genre_id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE
-);
-
-CREATE TABLE movie_genres (
-    movie_id INT,
-    genre_id INT,
-    PRIMARY KEY(movie_id, genre_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(movie_id),
-    FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
-);
-
-CREATE TABLE ratings (
-    user_id INT,
-    movie_id INT,
-    rating FLOAT,
-    timestamp BIGINT,
-    PRIMARY KEY(user_id, movie_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(movie_id)
+CREATE TABLE IF NOT EXISTS ratings (
+    user_id INTEGER,
+    movie_id INTEGER,
+    rating REAL,
+    timestamp INTEGER
 );
